0cd587a4bead348d6349b33c0d9e673d8ff42164
Darthan <darthan.sevenz@gmail.com>
2019-09-13 14:54:05 +0700

    Add classStudent method student model

--- application/app/Sitri/Models/Admin/Student.php
+++ application/app/Sitri/Models/Admin/Student.php
@@ -13,4 +13,9 @@ class Student extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function classStudents()
+    {
+        return $this->hasMany(ClassStudent::class);
+    }
 }
