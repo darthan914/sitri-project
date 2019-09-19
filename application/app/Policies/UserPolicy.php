49a49ded77b7f19fbcff1c1163e96ffc51b19383
Darthan <darthan.sevenz@gmail.com>
2019-09-19 17:03:50 +0700

    Create UserPolicy.php

---
+++ application/app/Policies/UserPolicy.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function list(User $auth)
+    {
+        return $auth->hasAccess('list-user');
+    }
+
+    public function create(User $auth)
+    {
+        return $auth->hasAccess('create-user');
+    }
+
+    public function update(User $auth, User $user)
+    {
+        return $auth->hasAccess('update-user') && ($auth->id === $user->id || $auth->hasAccess('full-user'));
+    }
+
+    public function delete(User $auth, User $user)
+    {
+        return $auth->hasAccess('delete-user') && $auth->id != $user->id;
+    }
+
+    public function full(User $auth)
+    {
+        return $auth->hasAccess('full-user');
+    }
+}
