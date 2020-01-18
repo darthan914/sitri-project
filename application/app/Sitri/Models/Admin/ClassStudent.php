d9543c11aa9fe76c346e6ee61e81dd7105dd960e
Darthan <darthan.sevenz@gmail.com>
2020-01-18 20:22:18 +0700

    Remove global scope class schedule

--- application/app/Sitri/Models/Admin/ClassStudent.php
+++ application/app/Sitri/Models/Admin/ClassStudent.php
@@ -12,18 +12,6 @@ class ClassStudent extends Model
 {
     protected $fillable = ['student_id', 'class_schedule_id', 'teacher_name'];
 
-    protected static function boot()
-    {
-        parent::boot();
-
-//        static::addGlobalScope('isActive', function (Builder $builder) {
-//            $builder->whereHas('student', function (Builder $student) {
-//                $student->whereHas('user', function (Builder $user) {
-//                    $user->where('active', 1);
-//                });
-//            });
-//        });
-    }
 
     public function classSchedule()
     {
