7b208ff0703cfff9fcb6c59b18be1e413e228144
Darthan <darthan.sevenz@gmail.com>
2020-01-18 19:46:36 +0700

    Refactor ClassRoomRepository

--- application/app/Sitri/Repositories/ClassRoom/ClassRoomRepository.php
+++ application/app/Sitri/Repositories/ClassRoom/ClassRoomRepository.php
@@ -8,32 +8,55 @@ use App\Sitri\Models\Admin\ClassRoom;
 
 class ClassRoomRepository implements ClassRoomRepositoryInterface
 {
-
     /**
-     * @return mixed
+     * @param array $with
+     *
+     * @return array
      */
-    public function all()
+    public function all(array $with = [])
     {
-        return ClassRoom::query()->orderBy('name')->get();
+        return ClassRoom::query()->with($with)->orderBy('name')->get()->toArray();
     }
 
     /**
-     * @param array $data
+     * @param int   $classRoomId
+     * @param array $with
      *
-     * @return mixed
+     * @return array
      */
-    public function getByRequest(array $data)
+    public function find($classRoomId, array $with = [])
     {
-        return ClassRoom::query()->orderBy('name')->get();
+        return ClassRoom::query()->with($with)->find($classRoomId)->toArray();
+    }
+
+    /**
+     * @param array $request
+     * @param array $with
+     *
+     * @return array
+     */
+    public function getByRequest(array $request, array $with = [])
+    {
+        return ClassRoom::query()->with($with)->orderBy('name')->get()->toArray();
     }
 
     /**
      * @param bool $active
      *
-     * @return mixed
+     * @return array
      */
-    public function getIsActive($active)
+    public function getActive($active = true)
     {
-        return ClassRoom::query()->where('active', $active)->orderBy('name')->get();
+        return ClassRoom::query()->where('active', $active)->orderBy('name')->get()->toArray();
+    }
+
+    /**
+     * @param int $classRoomId
+     *
+     * @return int
+     */
+    public function getMaxStudent($classRoomId)
+    {
+        return $this->find($classRoomId)['max_student'];
     }
 }
--- application/app/Sitri/Repositories/ClassRoom/ClassRoomRepositoryInterface.php
+++ application/app/Sitri/Repositories/ClassRoom/ClassRoomRepositoryInterface.php
@@ -7,7 +7,43 @@ namespace App\Sitri\Repositories\ClassRoom;
 use App\Sitri\Interfaces\ActiveInterface;
 use App\Sitri\Interfaces\DataInterface;
 
-interface ClassRoomRepositoryInterface extends DataInterface, ActiveInterface
+interface ClassRoomRepositoryInterface
 {
+    /**
+     * @param array $with
+     *
+     * @return array
+     */
+    public function all(array $with = []);
+
+    /**
+     * @param int   $classRoomId
+     * @param array $with
+     *
+     * @return array
+     */
+    public function find($classRoomId, array $with = []);
+
+    /**
+     * @param array $request
+     * @param array $with
+     *
+     * @return array
+     */
+    public function getByRequest(array $request, array $with = []);
+
+    /**
+     * @param bool $active
+     *
+     * @return array
+     */
+    public function getActive($active = true);
+
+    /**
+     * @param int $classRoomId
+     *
+     * @return int
+     */
+    public function getMaxStudent($classRoomId);
 
 }
