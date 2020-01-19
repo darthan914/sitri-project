61c5e52f7abf12d092fa4a49185705b8dea1a724
Darthan <darthan.sevenz@gmail.com>
2020-01-19 09:05:18 +0700

    Create button schedule align right

--- application/resources/views/admin/schedule/index.blade.php
+++ application/resources/views/admin/schedule/index.blade.php
@@ -51,7 +51,7 @@
     <div class="row">
         <div class="col-xs-12">
             <div class="box">
-                <div class="box-body">
+                <div class="box-body text-right">
                     <a href="{{ route('admin.schedule.create') }}" class="btn btn-default">Create</a>
                 </div>
                 <div class="box-body">
