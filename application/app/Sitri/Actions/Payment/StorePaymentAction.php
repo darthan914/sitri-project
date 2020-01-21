4b5b0870358bc5d6a07740716200ec5e0111d1e0
Darthan <darthan.sevenz@gmail.com>
2020-01-21 15:08:11 +0700

    Process item data for insert to payments

--- application/app/Sitri/Actions/Payment/StorePaymentAction.php
+++ application/app/Sitri/Actions/Payment/StorePaymentAction.php
@@ -5,6 +5,7 @@ namespace App\Sitri\Actions\Payment;
 
 
 use App\Sitri\Models\Admin\Payment;
+use App\Sitri\Repositories\Item\ItemRepositoryInterface;
 use Exception;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
@@ -12,12 +13,41 @@ use Illuminate\Database\Eloquent\Model;
 class StorePaymentAction
 {
     /**
+     * @var ItemRepositoryInterface
+     */
+    private $itemRepository;
+
+    /**
+     * StorePaymentAction constructor.
+     *
+     * @param ItemRepositoryInterface $itemRepository
+     */
+    public function __construct(ItemRepositoryInterface $itemRepository)
+    {
+        $this->itemRepository = $itemRepository;
+    }
+
+    /**
      * @param array $request
      *
      * @return Builder|Model
      */
     public function execute(array $request)
     {
+        $items = [];
+        foreach ($request['item'] as $key => $item) {
+            $getItem = $this->itemRepository->getByName($item);
+            if ($getItem) {
+                $items[] = [
+                    'name'     => $item,
+                    'value'    => $getItem['value'],
+                    'quantity' => $request['quantity'][$key]
+                ];
+            }
+        }
+        $request['items'] = $items;
+        $request['no_payment'] = time();
+
         return Payment::query()->create($request);
     }
 }
